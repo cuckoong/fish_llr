ed08e2ebc90522a946780e951d650a670de3109d
Pak <cuckoong@users.noreply.github.com>
2021-05-22 11:47:11 +0800

    Add files via upload

--- classification_visualization.py
+++ classification_visualization.py
@@ -3,57 +3,66 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    df = pd.read_csv('/Users/panpan/PycharmProjects/FIsh/nb.csv')
-    labels = ['0', '1', '2.5']
-    day = 5
-    baseline = df[(df['time']=='baseline') & (df['day']==day)]['acc']
-    min_1 = df[(df['time']=='1') & (df['day']==day)]['acc']
-    min_2 = df[(df['time']=='2') & (df['day']==day)]['acc']
-    min_30 = df[(df['time']=='30') & (df['day']==day)]['acc']
-
-    baseline_pvalue = df[(df['time'] == 'baseline') & (df['day'] == day)]['pvalue']
-    min_1_pvalue = df[(df['time'] == '1') & (df['day'] == day)]['pvalue']
-    min_2_pvalue = df[(df['time'] == '2') & (df['day'] == day)]['pvalue']
-    min_30_pvalue = df[(df['time'] == '30') & (df['day'] == day)]['pvalue']
-
+    method = 'KNN'
+    df = pd.read_csv('/Users/panpan/PycharmProjects/FIsh/Results/{}.csv'.format(method))
+    labels = ['0W', '1W', '2.5W']
     x = np.arange(len(labels))
-
     # the label locations
     width = 0.35  # the width of the bars
+    days = [5,6,7,8]
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(7, 7))#, sharey=True, sharex=True)
+    axs = axs.flatten()
 
-    fig, ax = plt.subplots()
-    rects1 = ax.bar(x - 2 * width / 3, baseline, width/3, label='baseline')
-    rects2 = ax.bar(x - 1 * width / 3, min_1, width/3, label='1min')
-    rects3 = ax.bar(x + 0 * width / 3, min_2, width/3, label='2min')
-    rects4 = ax.bar(x + 1 * width / 3, min_30, width/3, label='30min')
+    for i, day in enumerate(days):
+        baseline = df[(df['time']=='baseline') & (df['day']==day)]['acc']
+        min_1 = df[(df['time']=='1') & (df['day']==day)]['acc']
+        min_2 = df[(df['time']=='2') & (df['day']==day)]['acc']
+        min_30 = df[(df['time']=='30') & (df['day']==day)]['acc']
 
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Balanced Accuracy')
-    ax.set_title('Accuracy for fish on {}dpf (NB)'.format(day))
-    ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.legend()
+        baseline_pvalue = df[(df['time'] == 'baseline') & (df['day'] == day)]['pvalue']
+        min_1_pvalue = df[(df['time'] == '1') & (df['day'] == day)]['pvalue']
+        min_2_pvalue = df[(df['time'] == '2') & (df['day'] == day)]['pvalue']
+        min_30_pvalue = df[(df['time'] == '30') & (df['day'] == day)]['pvalue']
 
-    s = 12
-    p_t = 0.05
-    ax.scatter(x - 2 * width / 3, (baseline_pvalue <p_t) * (baseline+0.02), marker='*', c='k', s=s)
-    ax.scatter(x - 1 * width / 3, (min_1_pvalue <p_t) * (min_1+0.02), marker='*', c='k', s=s)
-    ax.scatter(x + 0 * width / 3, (min_2_pvalue <p_t) * (min_2+0.02), marker='*', c='k', s=s)
-    ax.scatter(x + 1 * width / 3, (min_30_pvalue <p_t) * (min_30+0.02), marker='*', c='k', s=s)
+        rects1 = axs[i].bar(x - 2 * width / 3, baseline, width/3, label='baseline')
+        rects2 = axs[i].bar(x - 1 * width / 3, min_1, width/3, label='1min')
+        rects3 = axs[i].bar(x + 0 * width / 3, min_2, width/3, label='2min')
+        rects4 = axs[i].bar(x + 1 * width / 3, min_30, width/3, label='30min')
 
-    p_t = 0.01
-    ax.scatter(x - 2 * width / 3, (baseline_pvalue < p_t) * (baseline + 0.04), marker='*', c='k', s=s)
-    ax.scatter(x - 1 * width / 3, (min_1_pvalue < p_t) * (min_1 + 0.04), marker='*', c='k', s=s)
-    ax.scatter(x + 0 * width / 3, (min_2_pvalue < p_t) * (min_2 + 0.04), marker='*', c='k', s=s)
-    ax.scatter(x + 1 * width / 3, (min_30_pvalue < p_t) * (min_30 + 0.04), marker='*', c='k', s=s)
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        axs[i].set_ylabel('Balanced Accuracy')
+        # axs[i].set_title('Accuracy for fish on {}dpf (SVM)'.format(day))
+        axs[i].set_xticks(x)
+        axs[i].set_xticklabels(labels)
+        axs[i].set_ylim((0, 1))
+        axs[i].legend(loc='upper left')
+        axs[i].annotate(xy = (1,0.8), text = 'Data from {}dpf'.format(day))
+        axs[i].axhline(y=0.5, ls='--', c='k')
 
-    p_t = 0.001
-    ax.scatter(x - 2 * width / 3, (baseline_pvalue < p_t) * (baseline + 0.06), marker='*', c='k', s=s)
-    ax.scatter(x - 1 * width / 3, (min_1_pvalue < p_t) * (min_1 + 0.06), marker='*', c='k', s=s)
-    ax.scatter(x + 0 * width / 3, (min_2_pvalue < p_t) * (min_2 + 0.06), marker='*', c='k', s=s)
-    ax.scatter(x + 1 * width / 3, (min_30_pvalue < p_t) * (min_30 + 0.06), marker='*', c='k', s=s)
+
+        s = 12
+        p_t = 0.05
+        axs[i].scatter(x - 2 * width / 3, (baseline_pvalue <p_t) * (baseline+0.02), marker='*', c='k', s=s)
+        axs[i].scatter(x - 1 * width / 3, (min_1_pvalue <p_t) * (min_1+0.02), marker='*', c='k', s=s)
+        axs[i].scatter(x + 0 * width / 3, (min_2_pvalue <p_t) * (min_2+0.02), marker='*', c='k', s=s)
+        axs[i].scatter(x + 1 * width / 3, (min_30_pvalue <p_t) * (min_30+0.02), marker='*', c='k', s=s)
+
+        p_t = 0.01
+        axs[i].scatter(x - 2 * width / 3, (baseline_pvalue < p_t) * (baseline + 0.04), marker='*', c='k', s=s)
+        axs[i].scatter(x - 1 * width / 3, (min_1_pvalue < p_t) * (min_1 + 0.04), marker='*', c='k', s=s)
+        axs[i].scatter(x + 0 * width / 3, (min_2_pvalue < p_t) * (min_2 + 0.04), marker='*', c='k', s=s)
+        axs[i].scatter(x + 1 * width / 3, (min_30_pvalue < p_t) * (min_30 + 0.04), marker='*', c='k', s=s)
+
+        p_t = 0.001
+        axs[i].scatter(x - 2 * width / 3, (baseline_pvalue < p_t) * (baseline + 0.06), marker='*', c='k', s=s)
+        axs[i].scatter(x - 1 * width / 3, (min_1_pvalue < p_t) * (min_1 + 0.06), marker='*', c='k', s=s)
+        axs[i].scatter(x + 0 * width / 3, (min_2_pvalue < p_t) * (min_2 + 0.06), marker='*', c='k', s=s)
+        axs[i].scatter(x + 1 * width / 3, (min_30_pvalue < p_t) * (min_30 + 0.06), marker='*', c='k', s=s)
+
+    if method == 'NB':
+        fig.suptitle('Classification Using {}'.format('Naive Bayes'), fontsize=16)
+    else:
+        fig.suptitle('Classification Using {}'.format(method), fontsize=16)
 
     fig.tight_layout()
-    plt.ylim((0, 0.9))
-    plt.axhline(y=0.5, ls = '--', c = 'k')
-    plt.show()
+    plt.savefig(method+'.png', dpi=600)
--- clustering.py
+++ clustering.py
@@ -6,44 +6,56 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_validate, cross_val_score, GridSearchCV
 from sklearn.model_selection import RepeatedStratifiedKFold,permutation_test_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 
 
-def svm_clf(data, label):
-    steps = [('scaler', StandardScaler()), ('SVM', SVC())]
-    svm_pip = Pipeline(steps)
+def clf(data, label, case='SVM'):
+    if case == 'SVM':
+        steps = [('scaler', StandardScaler()), ('SVM', SVC())]
+        pip = Pipeline(steps)
+        param_grid = [{'SVM__C': np.logspace(-3, 3, 7),
+                       'SVM__gamma': np.logspace(-3, 0, 4)}]
 
-    param_grid = [{'SVM__C': np.logspace(-3, 3, 7),
-                   'SVM__gamma': np.logspace(-3, 0, 4)}]
+    elif case == 'KNN':
+        steps = [('scaler', StandardScaler()), ('knn', KNeighborsClassifier())]
+        pip = Pipeline(steps)
+        param_grid = [{'knn__n_neighbors':np.arange(1,10)}]
 
-    gd_sr = GridSearchCV(svm_pip,
-                         param_grid=param_grid,
-                         scoring='balanced_accuracy',
-                         cv=5)
+    elif case == 'NB':
+        steps = [('scaler', StandardScaler()), ('NB', GaussianNB())]
+        pip = Pipeline(steps)
+        # permutation test
+        cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=1)
+        acc, _, pvalue = permutation_test_score(pip, data, label, cv=cv, n_permutations=100, n_jobs=-1,
+                                                random_state=1, verbose=0, scoring=('balanced_accuracy'))
+        return acc, pvalue
+
+    gd_sr = GridSearchCV(pip, param_grid=param_grid, scoring='balanced_accuracy', cv=5, n_jobs=-1)
     gd_sr.fit(data, label)
-
-    # print("The best classifier is: ", grid.best_estimator_)
     params = gd_sr.best_params_
-    best_C = params['SVM__C']
-    best_gamma = params['SVM__gamma']
-    print(best_C, best_gamma)
 
-    # best model
-    model = SVC(C=best_C, gamma=best_gamma)
-    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=1)
-    # results = cross_validate(model, data, label, cv = cv, scoring=('balanced_accuracy'))
-    # acc = results['test_score']
+    if case == 'SVM':
+        best_C = params['SVM__C']
+        best_gamma = params['SVM__gamma']
+        # best model
+        model = SVC(C=best_C, gamma=best_gamma)
+    elif case == 'KNN':
+        n_neighbors = params['knn__n_neighbors']
+        model = KNeighborsClassifier(n_neighbors=n_neighbors)
 
     # permutation test
-    acc, _, pvalue = permutation_test_score(model, data, label, cv=cv, n_permutations=100,
+    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=1)
+    acc, _, pvalue = permutation_test_score(model, data, label, cv=cv, n_permutations=100,n_jobs=-1,
                                            random_state=1, verbose=0, scoring=('balanced_accuracy'))
     return acc, pvalue
 
 
 if __name__ == "__main__":
-    powers = [0, 1, 2.5]
+    case = 'NB'
+    powers = [0, 1]#, 2.5]
     times = ['baseline', 1, 2, 30]
     days = [5, 6, 7, 8]
 
@@ -53,23 +65,20 @@ if __name__ == "__main__":
     acc_list = []
     pvalue_list = []
 
+
     for power in powers:
         for time in times:
             for day in days:
-                # power = 1
-                # time = 30
-                # day = 8
+                data_01 = np.load('C:\\Users\\xiaoliwu\\PycharmProjects\\Fish\\data2\\'+str(power)+'W-60h-'+str(day)+'dpf-01.xls-'+str(time)+'min-feature.npy')
+                data_02 = np.load('C:\\Users\\xiaoliwu\\PycharmProjects\\Fish\\data2\\'+str(power)+'W-60h-'+str(day)+'dpf-02.xls-'+str(time)+'min-feature.npy')
 
-                data_01 = np.load('/Users/panpan/PycharmProjects/FIsh/data/'+str(power)+'W-60h-'+str(day)+'dpf-01-'+str(time)+'min-feature.npy')
-                data_02 = np.load('/Users/panpan/PycharmProjects/FIsh/data/'+str(power)+'W-60h-'+str(day)+'dpf-02-'+str(time)+'min-feature.npy')
-
-                label_01 = np.load('/Users/panpan/PycharmProjects/FIsh/data/'+str(power)+'W-60h-'+str(day)+'dpf-01-label.npy')
-                label_02 = np.load('/Users/panpan/PycharmProjects/FIsh/data/'+str(power)+'W-60h-'+str(day)+'dpf-02-label.npy')
+                label_01 = np.load('C:\\Users\\xiaoliwu\\PycharmProjects\\Fish\\data2\\'+str(power)+'W-60h-'+str(day)+'dpf-01.xls-label.npy')
+                label_02 = np.load('C:\\Users\\xiaoliwu\\PycharmProjects\\Fish\\data2\\'+str(power)+'W-60h-'+str(day)+'dpf-02.xls-label.npy')
 
                 data = np.concatenate((data_01, data_02), axis=0)
                 label = np.concatenate((label_01, label_02), axis=0)
 
-                acc, pvalue = svm_clf(data, label)
+                acc, pvalue = clf(data, label, case=case)
                 power_list.append(power)
                 time_list.append(time)
                 day_list.append(day)
@@ -77,6 +86,6 @@ if __name__ == "__main__":
                 pvalue_list.append(pvalue)
 
     df = pd.DataFrame(data = dict(power = power_list, time = time_list, day = day_list, acc = acc_list, pvalue = pvalue_list))
-    df.to_csv('SVM.csv')
+    df.to_csv(case+'2.csv')
 
 
