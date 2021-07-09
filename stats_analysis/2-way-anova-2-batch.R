99b7571314774170f25333fa75ce7369b7a0236c
panda <spikeliuliu@gmail.com>
2021-07-10 02:23:47 +0800

    Data Preprocessing for two batch file and visualization (Not FINISH yet)

---
+++ stats_analysis/2-way-anova-2-batch.R
@@ -0,0 +1,101 @@
+# 2 way anova to analyse the radiation vs control
+# seperate day
+set.seed(345)
+# load data from files
+library(readr)
+library(dplyr)
+library(plotrix)
+
+integrate_df <- function(file, batch_num, selected_day, integrate_duration=60){
+  myData <- read_csv(file)
+  myData <- myData %>%
+    filter(day == selected_day) %>%
+    select(burdur, animal, end, label, day) %>%
+    mutate(animal = as.factor(animal),
+       label = as.factor(label),
+       day = as.factor(day)) %>%
+    mutate(inte_end = (end-1) %/% integrate_duration + 1) %>%
+    group_by(day, label, animal, inte_end) %>%
+    summarise(inte_burdur = sum(burdur)) %>%
+    mutate(batch = batch_num) %>%
+    mutate(animal_batch = paste(batch, animal, sep='-')) %>%
+    mutate(animal_batch = as.factor(animal_batch), batch = as.factor(batch),
+           label = as.factor(label), inte_end = as.factor(inte_end)) %>%
+    ungroup() %>%
+    select(animal_batch, batch, label, inte_burdur, inte_end)
+  return(myData)
+}
+
+file1 <- '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_batch1_burst4.csv'
+file2 <- '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_batch2_burst4.csv'
+myData1 <- integrate_df(file1, batch_num = 1, selected_day = 5)
+myData2 <- integrate_df(file2, batch_num = 2, selected_day = 5)
+
+# visualize all batch data
+myData <- rbind(myData1, myData2)
+
+Group_Data <- myData %>%
+  group_by(label, inte_end, batch) %>%
+  summarise(mean_burdur = mean(inte_burdur),
+            sd_burdur = std.error(inte_burdur))
+
+# visualize individual fish activity
+library(ggplot2)
+ggplot(data = Group_Data, aes(x=inte_end, y=mean_burdur, group = label, color = label)) +
+  geom_pointrange(aes(ymin=mean_burdur-sd_burdur, ymax=mean_burdur+sd_burdur)) +
+  geom_line() + facet_grid(.~batch)
+
+
+  mutate(stim_stage = case_when(inte_end <= 30 ~ inte_end,
+                              inte_end > 30 & inte_end <= 90 ~ inte_end,
+                              inte_end > 90 & inte_end <= 150 ~ inte_end - 60)) %>%
+  mutate(stim_stage = as.factor(stim_stage),
+         batch = as.factor(1)) %>%
+  select(inte_burdur, stim_stage, animal, label, day, batch)
+
+myData <-  rbind(myData1, myData2)
+
+myData <- read_csv('/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_burst4.csv')
+myData <-  myData %>%
+  mutate(animal = as.factor(animal), label = as.factor(label), day = as.factor(day),
+         stim_stage = as.factor(stim_stage), batch = as.factor(batch)) %>%
+  select(inte_burdur, stim_stage, animal, label, day, batch)
+
+# 2 way anova with repeated measure
+library(nlme)
+
+# burdur, aname, label, day, stim_stage, stim_group
+baseline <-  lme(inte_burdur~1, random=~1|animal/stim_stage/day/batch, data = myData, method='ML')
+# save model
+dir <-  '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/Statistical_Results/5W'
+save(baseline, file= paste(dir,'lme_burst4_baseline_integrate.rda', sep="/"))
+
+# load('lme_burst4_baseline.rda')
+radiationM <- update(baseline, .~. + label)
+save(radiationM, file= paste(dir,'lme_burst4_radiationM.rda', sep="/"))
+
+dayM <- update(radiationM, .~. + day)
+save(dayM, file= paste(dir,'lme_burst4_dayM.rda', sep="/"))
+
+batchM <- update(dayM, .~. + batch)
+save(batchM, file= paste(dir,'lme_burst4_stim_batchM.rda', sep="/"))
+
+stim_stageM <- update(batchM, .~. + stim_stage)
+save(stim_stageM, file= paste(dir,'lme_burst4_stim_stageM.rda', sep="/"))
+
+# stim_groupM <- update(stim_stageM, .~. + stim_group)
+radiation_day <- update(batchM, .~. + label:day)
+save(radiation_day, file= paste(dir, 'lme_burst4_radiation_day.rda', sep="/"))
+
+radiation_stim <- update(radiation_day, .~. + label:stim_stage)
+save(radiation_stim, file= paste(dir, 'lme_burst4_radiation_stim.rda', sep="/"))
+
+day_stim <- update(radiation_stim, .~. + day:stim_stage)
+save(day_stim, file= paste(dir, 'lme_burst4_day_stim.rda', sep="/"))
+
+all <- update(day_stim, .~. + day:stim_stage:label)
+
+save(all, file= paste(dir, 'lme_burst4_all.rda', sep="/"))
+
+# visualization
+anova(baseline, radiationM, dayM, stim_stageM, radiation_day, radiation_stim)#, day_stim, all)
