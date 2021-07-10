99c2dff7da0cc3d3a8150d88dc7e6f823c9ad489
panda <spikeliuliu@gmail.com>
2021-07-10 11:01:59 +0800

    Data Preprocessing for two batch file and visualization (correct mistakes from csv files)

--- stats_analysis/2-way-anova-2-batch.R
+++ stats_analysis/2-way-anova-2-batch.R
@@ -6,6 +6,10 @@ library(readr)
 library(dplyr)
 library(plotrix)
 
+minmax_scale <- function(x){
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
 integrate_df <- function(file, batch_num, selected_day, integrate_duration=60){
   myData <- read_csv(file)
   myData <- myData %>%
@@ -17,85 +21,78 @@ integrate_df <- function(file, batch_num, selected_day, integrate_duration=60){
     mutate(inte_end = (end-1) %/% integrate_duration + 1) %>%
     group_by(day, label, animal, inte_end) %>%
     summarise(inte_burdur = sum(burdur)) %>%
+    ungroup() %>%
+    group_by(day, label, animal) %>%
+    mutate(minmax_bur = minmax_scale(inte_burdur)) %>%
     mutate(batch = batch_num) %>%
     mutate(animal_batch = paste(batch, animal, sep='-')) %>%
-    mutate(animal_batch = as.factor(animal_batch), batch = as.factor(batch),
-           label = as.factor(label), inte_end = as.factor(inte_end)) %>%
+    mutate(animal_batch = as.factor(animal_batch),
+           batch = as.factor(batch),
+           label = as.factor(label)) %>%
     ungroup() %>%
-    select(animal_batch, batch, label, inte_burdur, inte_end)
+    select(animal_batch, batch, label,inte_burdur, minmax_bur, inte_end)
   return(myData)
 }
 
 file1 <- '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_batch1_burst4.csv'
 file2 <- '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_batch2_burst4.csv'
-myData1 <- integrate_df(file1, batch_num = 1, selected_day = 5)
-myData2 <- integrate_df(file2, batch_num = 2, selected_day = 5)
+selected_day <- 6
+myData1 <- integrate_df(file1, batch_num = 1, selected_day = selected_day)
+myData2 <- integrate_df(file2, batch_num = 2, selected_day = selected_day)
 
 # visualize all batch data
 myData <- rbind(myData1, myData2)
 
-Group_Data <- myData %>%
-  group_by(label, inte_end, batch) %>%
-  summarise(mean_burdur = mean(inte_burdur),
-            sd_burdur = std.error(inte_burdur))
+# Group_Data <- myData %>%
+#   group_by(label, inte_end, batch) %>%
+#   summarise(mean_burdur = mean(inte_burdur),
+#             sd_burdur = std.error(inte_burdur))
+#
+# # visualize individual fish activity
+# library(ggplot2)
+# ggplot(data = Group_Data, aes(x=inte_end, y=mean_burdur, group = label, color = label)) +
+#   geom_pointrange(aes(ymin=mean_burdur-sd_burdur, ymax=mean_burdur+sd_burdur)) +
+#   geom_line() + facet_grid(.~batch)
 
-# visualize individual fish activity
-library(ggplot2)
-ggplot(data = Group_Data, aes(x=inte_end, y=mean_burdur, group = label, color = label)) +
-  geom_pointrange(aes(ymin=mean_burdur-sd_burdur, ymax=mean_burdur+sd_burdur)) +
-  geom_line() + facet_grid(.~batch)
 
-
-  mutate(stim_stage = case_when(inte_end <= 30 ~ inte_end,
-                              inte_end > 30 & inte_end <= 90 ~ inte_end,
-                              inte_end > 90 & inte_end <= 150 ~ inte_end - 60)) %>%
-  mutate(stim_stage = as.factor(stim_stage),
-         batch = as.factor(1)) %>%
-  select(inte_burdur, stim_stage, animal, label, day, batch)
-
-myData <-  rbind(myData1, myData2)
-
-myData <- read_csv('/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_burst4.csv')
 myData <-  myData %>%
-  mutate(animal = as.factor(animal), label = as.factor(label), day = as.factor(day),
-         stim_stage = as.factor(stim_stage), batch = as.factor(batch)) %>%
-  select(inte_burdur, stim_stage, animal, label, day, batch)
+  mutate(stim_stage = case_when(inte_end <= 90 ~ inte_end,
+                                inte_end > 90 ~ (inte_end - 60))) %>%
+  mutate(stim_stage = as.factor(stim_stage))
+
 
 # 2 way anova with repeated measure
 library(nlme)
+# save model
+dir <-  paste('/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/Statistical_Results/5W/day',selected_day, sep='')
 
 # burdur, aname, label, day, stim_stage, stim_group
-baseline <-  lme(inte_burdur~1, random=~1|animal/stim_stage/day/batch, data = myData, method='ML')
-# save model
-dir <-  '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/Statistical_Results/5W'
+baseline <-  lme(inte_burdur~1, random=~1|animal_batch/stim_stage, data = myData, method='ML',
+                 control=(msMaxIter=100))
 save(baseline, file= paste(dir,'lme_burst4_baseline_integrate.rda', sep="/"))
 
 # load('lme_burst4_baseline.rda')
 radiationM <- update(baseline, .~. + label)
 save(radiationM, file= paste(dir,'lme_burst4_radiationM.rda', sep="/"))
 
-dayM <- update(radiationM, .~. + day)
-save(dayM, file= paste(dir,'lme_burst4_dayM.rda', sep="/"))
-
-batchM <- update(dayM, .~. + batch)
-save(batchM, file= paste(dir,'lme_burst4_stim_batchM.rda', sep="/"))
+batchM <- update(radiationM, .~. + batch)
+save(batchM, file= paste(dir,'lme_burst4_batchM.rda', sep="/"))
 
 stim_stageM <- update(batchM, .~. + stim_stage)
 save(stim_stageM, file= paste(dir,'lme_burst4_stim_stageM.rda', sep="/"))
 
 # stim_groupM <- update(stim_stageM, .~. + stim_group)
-radiation_day <- update(batchM, .~. + label:day)
-save(radiation_day, file= paste(dir, 'lme_burst4_radiation_day.rda', sep="/"))
+radiation_batch <- update(stim_stageM, .~. + label:batch)
+save(radiation_batch, file= paste(dir, 'lme_burst4_radiation_batch.rda', sep="/"))
 
-radiation_stim <- update(radiation_day, .~. + label:stim_stage)
+radiation_stim <- update(radiation_batch, .~. + label:stim_stage)
 save(radiation_stim, file= paste(dir, 'lme_burst4_radiation_stim.rda', sep="/"))
 
-day_stim <- update(radiation_stim, .~. + day:stim_stage)
-save(day_stim, file= paste(dir, 'lme_burst4_day_stim.rda', sep="/"))
+batch_stim <- update(radiation_stim, .~. + batch:stim_stage)
+save(batch_stim, file= paste(dir, 'lme_burst4_batch_stim.rda', sep="/"))
 
-all <- update(day_stim, .~. + day:stim_stage:label)
-
+all <- update(batch_stim, .~. + batch:stim_stage:label)
 save(all, file= paste(dir, 'lme_burst4_all.rda', sep="/"))
 
 # visualization
-anova(baseline, radiationM, dayM, stim_stageM, radiation_day, radiation_stim)#, day_stim, all)
+anova(baseline,radiationM, batchM, stim_stageM, radiation_batch, radiation_stim, batch_stim, all)
--- stats_analysis/data_cleaning.py
+++ stats_analysis/data_cleaning.py
@@ -8,8 +8,8 @@ Generate the burst duration from raw data for statistical analysis on light-indu
 '''
 
 if __name__ == '__main__':
-    feature_dir = '/home/tmp2/PycharmProjects/fish_llr/Data/burst4/5w_2/'
-    label_dir = '/home/tmp2/PycharmProjects/fish_llr/Data/burst4/5w_2/'
+    feature_dir = '/home/tmp2/PycharmProjects/fish_llr/Data/burst4/5w_1/'
+    label_dir = '/home/tmp2/PycharmProjects/fish_llr/Data/burst4/5w_1/'
     res_dir = '/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/'
     days = [5, 6, 7, 8]
     radiations = [5]
@@ -25,13 +25,13 @@ if __name__ == '__main__':
 
                 # get duration values
                 features = df[['burdur', 'aname', 'end']].copy(deep=True)
+                features.sort_values(['end', 'aname'], inplace=True)
                 features['animal'] = str(plate) + '-'+features['aname']
                 features['end'] = round(features['end']).astype('int')
 
                 label_file = '{}W-60h-{}dpf-0{}-label.npy'.format(radiation, day, plate)
                 # 1: radiation; 0: control
                 label = np.load(label_dir+label_file)
-
                 # extend the label array with no-fish well
                 # label = np.append(label, np.repeat(-1, 96-len(label)))
                 labels = np.tile(label, int(len(features)/len(label)))
@@ -42,14 +42,14 @@ if __name__ == '__main__':
                 features.loc[features['label'] == 0, 'radiation'] = 0
 
                 # result dir
-                res_file = 'burdur_r{}_d{}_p{}.csv'.format(radiation, day, plate)
-                features.to_csv(res_dir+res_file)
+                # res_file = 'burdur_r{}_d{}_p{}.csv'.format(radiation, day, plate)
+                # features.to_csv(res_dir+res_file)
 
                 # csv_file = r'C:\Users\xiaoliwu\PycharmProjects\Fish\stat_analysis\burdur_r{}_d{}_p{}.csv'.format(
                 # radiation, day, plate) features = pd.read_csv(csv_file)
                 df_list.append(features)
 
     frame = pd.concat(df_list, axis=0, ignore_index=True)
-    frame.to_csv('/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_batch2_burst4.csv')
+    frame.to_csv('/home/tmp2/PycharmProjects/fish_llr/Analysis_Results/stat_data/burdur_5w_batch1_burst4.csv')
 
 
