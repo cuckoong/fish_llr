d9eb626091f0c483807fd007d03f284d7e58a32a
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-12 17:50:29 +0800

    burst duration of raw sum activity

--- src/stats_visualize_burst.R
+++ src/stats_visualize_burst.R
@@ -40,79 +40,100 @@ integrate_df <- function(file, batch_num, selected_day, integrate_duration=60){
 setwd('/Users/panpan/PycharmProjects/old_project/fish_llr')
 
 BATCH <- 2  # 1 or 2
-POWER <- 1.2  # power of the burst activity
-ACTIVITY_TYPE <- 'all' #'burdur' # 'burdur' or 'all'
+POWER <- 1  # power of the burst activity
+ACTIVITY_TYPE <- 'burdur' #'burdur' # 'burdur' or 'all'
 
 file1 <- paste0('Processed_data/quantization/Tg/stat_data/', ACTIVITY_TYPE, '_', POWER, 'w_60h_batch', BATCH, '_burst4.csv')
 # file1 <- 'Processed_data/quantization/Tg/stat_data/all_1.2w_60h_batch1_burst4.csv'
-selected_day <- 5
+selected_day <-8
 
 # intergrate every 60s
 myData <- integrate_df(file1, batch_num = BATCH, selected_day = selected_day)
 
-
-# visualize individual fish data in 150min in line plot
-ggplot(data = myData, aes(x=inte_end, y=inte_activity_sum,
-                          group=animal_batch, color=radiation_label)) +
-  geom_point(alpha=0.3)+geom_line(alpha=0.3)
-
-# todo: ???? remove control????
-
-
-
-# remove baseline activity (0 ~ 30s)
-rm_baseline_Data <-  myData %>%
-  group_by(animal_batch) %>%
-  mutate(stim_baseline = case_when(inte_end <= 30 ~ 0,
-                                inte_end > 30 ~ 1)) %>%
-  # mutate(rm_activity_sum = (inte_activity_sum - mean(inte_activity_sum[stim_baseline==0]))) %>%
-   mutate(rm_activity_sum = (scaled_activity_sum - mean(scaled_activity_sum[stim_baseline==0]))) %>%   # minmax scaling
-  # filter(stim_baseline == 1) %>%
-  select(animal_batch, radiation_label, inte_end, rm_activity_sum)
-
-# group data with same radiation level and same time point
-Group_Data <- rm_baseline_Data %>%
-  group_by(radiation_label, inte_end) %>%
-  summarise(median_activity_sum = median(rm_activity_sum),
-            mean_activity_sum = mean(rm_activity_sum),
-            sd_activity_sum = sd(rm_activity_sum),
-            Q1 = quantile(rm_activity_sum, 0.25),
-            Q3 = quantile(rm_activity_sum, 0.75))
-
-# visualize individual fish activity
-# mean and sd activity
-ggplot(data = Group_Data, aes(x=inte_end, y=mean_activity_sum,
-                              group = radiation_label, color = radiation_label)) +
-  geom_line()+
-  # geom_pointrange(aes(ymin=Q1, ymax=Q3), alpha=0.5) +
-  geom_pointrange(aes(ymin=mean_activity_sum-sd_activity_sum, ymax=mean_activity_sum+sd_activity_sum)) +
+# visualize individual fish data in 150min in line plot with 25% and 75% quantile
+myData_grouped <- myData %>%
+  group_by(batch, radiation_label, inte_end) %>%
+  summarise(median_activity_sum = median(inte_activity_sum),
+            mean_activity_sum = mean(inte_activity_sum),
+            mean_activity_sum = mean(inte_activity_sum),
+            sd_activity_sum = sd(inte_activity_sum),
+            Q1 = quantile(inte_activity_sum, 0.25),
+            Q3 = quantile(inte_activity_sum, 0.75))
+ggplot(data = myData_grouped,
+       aes(x=inte_end,
+           y=median_activity_sum,
+           color=radiation_label)) +
+  geom_point(alpha=0.3)+
+  geom_line(alpha=0.3) +
+  geom_pointrange(aes(ymin=Q1,
+                      ymax=Q3,
+                      color=radiation_label), alpha=0.5) +
   ylab('Acitivty Duration (s)') + xlab('Tracking Time (min)') +
-  scale_color_discrete(name='', labels = c('Control', '1.2W'), breaks = c(0, 1)) +
+  scale_color_discrete(name='', labels = c('Control', paste0(POWER, 'W')),
+                       breaks = c(0, 1)) +
   theme(legend.position = 'right') +
   annotate("text", x = 15, y = 1, label = 'Baseline\nOFF', color = 'Blue') +
   annotate("text", x = c(45,105), y = 1, label = 'ON', color = 'Red') +
   annotate("text", x = c(75,135), y = 1, label = 'OFF', color = 'Blue') +
   geom_vline(xintercept = c(30, 60, 90, 120), linetype = 'dotted') +
   theme_publish()
-ggsave(paste0('Figures/Stats/Quantization/Tg/', POWER,
-              'W_day', selected_day, '_batch_', BATCH, 'mean.png'),
-       width=8, height=6, units='in', dpi=300)
-
-# median and IQR activity
-ggplot(data = Group_Data, aes(x=inte_end, y=median_activity_sum,
-                              group = radiation_label, color = radiation_label)) +
-  geom_line()+
-  geom_pointrange(aes(ymin=Q1, ymax=Q3), alpha=0.5) +
-  ylab('Activity Duration (s)') + xlab('Tracking Time (min)') +
-  scale_color_discrete(name='', labels = c('Control', '1.2W'), breaks = c(0, 1)) +
-  theme(legend.position = 'right') +
-  annotate("text", x = 15, y = 1, label = 'Baseline\nOFF', color = 'Blue') +
-  annotate("text", x = c(45,105), y = 1, label = 'ON', color = 'Red') +
-  annotate("text", x = c(75,135), y = 1, label = 'OFF', color = 'Blue') +
-  geom_vline(xintercept = c(30, 60, 90, 120), linetype = 'dotted') +
-  theme_publish()
-ggsave(paste0('Figures/Stats/Quantization/Tg/', POWER,
+ggsave(paste0('Figures/Stats/Quantization/Tg/burst/raw/', POWER,
               'W_day', selected_day, '_batch_', BATCH, 'median.png'),
        width=8, height=6, units='in', dpi=300)
-
-
+#
+# # remove baseline activity (0 ~ 30s)
+# rm_baseline_Data <-  myData %>%
+#   group_by(animal_batch) %>%
+#   mutate(stim_baseline = case_when(inte_end <= 30 ~ 0,
+#                                 inte_end > 30 ~ 1)) %>%
+#   # mutate(rm_activity_sum = (inte_activity_sum - mean(inte_activity_sum[stim_baseline==0]))) %>%
+#    mutate(rm_activity_sum = (scaled_activity_sum - mean(scaled_activity_sum[stim_baseline==0]))) %>%   # minmax scaling
+#   # filter(stim_baseline == 1) %>%
+#   select(animal_batch, radiation_label, inte_end, rm_activity_sum)
+#
+# # group data with same radiation level and same time point
+# Group_Data <- rm_baseline_Data %>%
+#   group_by(radiation_label, inte_end) %>%
+#   summarise(median_activity_sum = median(rm_activity_sum),
+#             mean_activity_sum = mean(rm_activity_sum),
+#             sd_activity_sum = sd(rm_activity_sum),
+#             Q1 = quantile(rm_activity_sum, 0.25),
+#             Q3 = quantile(rm_activity_sum, 0.75))
+#
+# # visualize individual fish activity
+# # mean and sd activity
+# ggplot(data = Group_Data, aes(x=inte_end, y=mean_activity_sum,
+#                               group = radiation_label, color = radiation_label)) +
+#   geom_line()+
+#   # geom_pointrange(aes(ymin=Q1, ymax=Q3), alpha=0.5) +
+#   geom_pointrange(aes(ymin=mean_activity_sum-sd_activity_sum, ymax=mean_activity_sum+sd_activity_sum)) +
+#   ylab('Acitivty Duration (s)') + xlab('Tracking Time (min)') +
+#   scale_color_discrete(name='', labels = c('Control', '1.2W'), breaks = c(0, 1)) +
+#   theme(legend.position = 'right') +
+#   annotate("text", x = 15, y = 1, label = 'Baseline\nOFF', color = 'Blue') +
+#   annotate("text", x = c(45,105), y = 1, label = 'ON', color = 'Red') +
+#   annotate("text", x = c(75,135), y = 1, label = 'OFF', color = 'Blue') +
+#   geom_vline(xintercept = c(30, 60, 90, 120), linetype = 'dotted') +
+#   theme_publish()
+# ggsave(paste0('Figures/Stats/Quantization/Tg/', POWER,
+#               'W_day', selected_day, '_batch_', BATCH, 'mean.png'),
+#        width=8, height=6, units='in', dpi=300)
+#
+# # median and IQR activity
+# ggplot(data = Group_Data, aes(x=inte_end, y=median_activity_sum,
+#                               group = radiation_label, color = radiation_label)) +
+#   geom_line()+
+#   geom_pointrange(aes(ymin=Q1, ymax=Q3), alpha=0.5) +
+#   ylab('Activity Duration (s)') + xlab('Tracking Time (min)') +
+#   scale_color_discrete(name='', labels = c('Control', '1.2W'), breaks = c(0, 1)) +
+#   theme(legend.position = 'right') +
+#   annotate("text", x = 15, y = 1, label = 'Baseline\nOFF', color = 'Blue') +
+#   annotate("text", x = c(45,105), y = 1, label = 'ON', color = 'Red') +
+#   annotate("text", x = c(75,135), y = 1, label = 'OFF', color = 'Blue') +
+#   geom_vline(xintercept = c(30, 60, 90, 120), linetype = 'dotted') +
+#   theme_publish()
+# ggsave(paste0('Figures/Stats/Quantization/Tg/', POWER,
+#               'W_day', selected_day, '_batch_', BATCH, 'median.png'),
+#        width=8, height=6, units='in', dpi=300)
+#
+#
