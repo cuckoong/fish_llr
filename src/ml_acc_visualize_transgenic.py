525c81d8e821b874709f75c3821c3eb76af01f9c
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-12 17:49:11 +0800

    change pvalue to p-value

--- src/ml_acc_visualize_transgenic.py
+++ src/ml_acc_visualize_transgenic.py
@@ -9,10 +9,10 @@ os.chdir('/Users/panpan/PycharmProjects/old_project/fish_llr')
 if __name__ == '__main__':
     mode = 'quantization'
     fish_type = 'Tg'
-    batch = 2
-    power = 1
-    df = pd.read_csv('Analysis_Results/ML_results/Tg/Quan_Data_Classification/feature_selection/{}W/acc-batch{}.csv'.
-                     format(power, batch))
+    batch = 1
+    power = 1.2
+    df_all = pd.read_csv('Analysis_Results/ML_results/Tg/Quan_Data_Classification/feature_selection/all.csv')
+    df = df_all[(df_all['batch'] == batch) & (df_all['power'] == power)].copy()
     # df['SAR'] = df['power'].map({0: 0, 5: 0.009, 1.2: 2, 3: 4.9})
     df['acc'] = df['acc'] * 100
     df['classifier_idx'] = df['case'].map({'NB': 2, 'SVM': 0, 'KNN': 1})
@@ -26,13 +26,13 @@ if __name__ == '__main__':
         sns.barplot(x='classifier_idx', y='acc', data=df_sel)
         # plt.bar(df_sel['classifier_idx'], df_sel['acc'], width=0.5, color='#1f77b4', align='edge')
         # sns.barplot(x='classifier', y='acc', data=df_sel)
-        # add stars to the top of the bars if pvalue less than 0.05
+        # add stars to the top of the bars if p-value less than 0.05
         for i, row in df_sel.iterrows():
-            if (row['pvalue'] < 0.05) and (row['acc'] > 60):
+            if (row['p-value'] < 0.05) and (row['acc'] > 60):
                 plt.text(row['classifier_idx'] - 0.1, row['acc'] + 0.5, '*', fontsize=10)
-            if (row['pvalue'] < 0.01) and (row['acc'] > 60):
+            if (row['p-value'] < 0.01) and (row['acc'] > 60):
                 plt.text(row['classifier_idx'] - 0.1, row['acc'] + 6, '*', fontsize=10)
-            if (row['pvalue'] < 0.001) and (row['acc'] > 60):
+            if (row['p-value'] < 0.001) and (row['acc'] > 60):
                 plt.text(row['classifier_idx'] - 0.1, row['acc'] + 12, '*', fontsize=10)
 
         plt.ylim(0, 100)
