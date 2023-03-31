7bfb406d558ae4300cc2ddd2209e7fac1036b345
cuckoong <wuxiaol7@connect.hku.hk>
2023-03-31 11:04:37 +0800

    fix: use timestamps to log the light intensity

--- src/video_extract.py
+++ src/video_extract.py
@@ -7,17 +7,21 @@ import matplotlib.pyplot as plt
 os.chdir('/Users/panpan/PycharmProjects/old_project/fish_llr')
 
 if __name__ == '__main__':
-    POWER = 1.2
+    POWER = 1
     BATCH = 2
     DAY = 8
+
+    pre_stim_duration = 3
+    post_stim_duration = 3
+    interval = 1
 
     video_path = f'Data/Quantization/Tg/{POWER}W-batch{BATCH}/{POWER}W-60h-{DAY}dpf-01.avi'
     background_mask_folder = f'Figures/background_image/{POWER}W-batch{BATCH}/{POWER}W-60h-{DAY}dpf-01'
 
     # start time and end time
-    stim_times = [1800, 3600, 5400, 7200]
-    start_times = [(t - 30) * 1000 for t in stim_times]  # pre_duration = 30s
-    end_times = [(t + 30) * 1000 for t in stim_times]  # post_duration = 30s
+    stim_times = [7200]
+    start_times = [(t - pre_stim_duration) * 1000 for t in stim_times]
+    end_times = [(t + post_stim_duration) * 1000 for t in stim_times]
 
     for i in range(len(start_times)):
         # get the start time and end time
@@ -25,7 +29,9 @@ if __name__ == '__main__':
         end_time = end_times[i]
 
         # create an array to store the intensity values
-        intensity = {}
+        intensity = []
+        timestamps = []
+        anmial_id = []
 
         # load the video
         cap = cv2.VideoCapture(video_path)
@@ -42,6 +48,8 @@ if __name__ == '__main__':
             # check if the frame is empty
             if not ret:
                 break
+
+            timestamp = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
 
             # convert the frame to grayscale
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -69,21 +77,21 @@ if __name__ == '__main__':
                 # log the intensity
                 masked_intensity = np.sum(gray_masked) / np.sum(mask / 255)
 
-                if mask_name not in intensity:
-                    intensity[mask_name] = [masked_intensity]
-                else:
-                    intensity[mask_name].append(masked_intensity)
+                intensity.append(masked_intensity)
+                timestamps.append(timestamp)
+                anmial_id.append(f'{BATCH}-1-{mask_name}')
 
-            if not ret or cap.get(cv2.CAP_PROP_POS_MSEC) > (end_time + 1000):
+            # need to skip frames to get 1 frame per second
+            cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + interval * fps)
+
+            if not ret or cap.get(cv2.CAP_PROP_POS_MSEC) > end_time:
                 # release the video capture object
                 cap.release()
                 cv2.destroyAllWindows()
                 break
 
-            # need to skip frames to get 1 frame per second
-            cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + fps)
-
         # FIGURE path
+        '''
         figure_path = f'Figures/light_intensity/Tg/{POWER}W-batch{BATCH}/{POWER}W-60h-{DAY}dpf-01/'
         if not os.path.exists(figure_path):
             os.makedirs(figure_path)
@@ -98,6 +106,7 @@ if __name__ == '__main__':
         for mask_name in intensity:
             plt.plot(intensity[mask_name] - intensity[mask_name][0], label=mask_name)
         plt.savefig(os.path.join(figure_path, f'{start_time}_{end_time}_rm_baseline.png'))
+        '''
 
         if not os.path.exists(f'Processed_data/light_intensity/Tg/{POWER}W-batch{BATCH}/{POWER}W-60h-{DAY}dpf-01'):
             os.makedirs(f'Processed_data/light_intensity/Tg/{POWER}W-batch{BATCH}/{POWER}W-60h-{DAY}dpf-01')
@@ -105,14 +114,10 @@ if __name__ == '__main__':
         # save the intensity to pickle
         # convert data to dataframe
         df_dict = {
-            'animal_id': [],
-            'end': [],
-            'light_intensity': []
+            'animal_id': anmial_id,
+            'timestamps': timestamps,
+            'light_intensity': intensity
         }
-        for key in intensity.keys():
-            df_dict['animal_id'].extend([f'{BATCH}-1-{key}'] * len(intensity[key]))
-            df_dict['end'].extend(np.arange(stim_times[i] - 30, stim_times[i] + 31, 1))
-            df_dict['light_intensity'].extend(intensity[key])
 
         df = pd.DataFrame(df_dict)
 
