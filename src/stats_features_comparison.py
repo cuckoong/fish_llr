a19977240bda0b263da827bfd09b58415594c15e
cuckoong <wuxiaol7@connect.hku.hk>
2023-04-01 19:12:44 +0800

    feat: add visualization and stats of features comparison

--- src/stats_features_comparison.py
+++ src/stats_features_comparison.py
@@ -1,117 +1,131 @@
 import os
-
-import pandas as pd
 import seaborn as sns
-from utils import group_batch_data
-from utils import rm_animal_baseline, rm_batch_baseline, rm_linear_trend
-from behaviour_features_utils import *
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.stats import mannwhitneyu, chi2_contingency, fisher_exact
 
-sns.set_style('whitegrid')
-sns.set_context('paper')
-sns.set_palette('Set2')
-
-os.chdir('/Users/panpan/PycharmProjects/old_project/fish_llr')
+sns.set(style="whitegrid")
 
 
-def get_compare_group(power_level):
-    if power_level == 0:  # 0W/Kg vertical, wt
-        groups = ['Control', '0W/Kg SAR']
-    elif power_level == 1:  # 1.6W/Kg vertical, tg
-        groups = ['control', '1.6W/Kg SAR']
-    elif power_level == 1.2:  # 2W/Kg vertical, wt+tg
-        groups = ['Control', '2W/Kg SAR']
-    elif power_level == 3:  # 4.9W/Kg vertical, wt
-        groups = ['Control', '4.9W/Kg SAR']
-    elif power_level == 5:  # horizontal, wt
-        groups = ['Control', '0.009W/Kg SAR']
-    return groups
+def auto_select_test(contingency_table):
+    chi2, p_value_chi2, _, _ = chi2_contingency(contingency_table)
+    row_totals = np.sum(contingency_table, axis=1)
+    col_totals = np.sum(contingency_table, axis=0)
+    grand_total = np.sum(contingency_table)
+
+    expected = np.outer(row_totals, col_totals) / grand_total
+    use_fisher = np.any(expected < 5)
+
+    if use_fisher:
+        odds_ratio, p_value_fisher = fisher_exact(contingency_table)
+        return "Fisher's exact test", odds_ratio, p_value_fisher
+    else:
+        return "Chi-squared test", None, p_value_chi2
+
+
+def significance_markers(value):
+    if value < 0.001:
+        return '***'
+    elif value < 0.01:
+        return '**'
+    elif value < 0.05:
+        return '*'
+    else:
+        return ''
+
+
+def compare_event(df, selected_column):
+    df['event'] = df[selected_column].apply(lambda x: 1 if pd.notna(x) else 0)
+
+    # visualize bar chart with label side by side, day as x-axis, and startle percentage as y-axis
+    event_summary = df.groupby(['label', 'day'])['event'].agg(['mean', 'sum', 'count']).reset_index()
+
+    # Calculate p-values and add stars to the plot
+    days = event_summary['day'].unique()
+    group1_data = event_summary[event_summary['label'] == 'Control']
+    group2_data = event_summary[event_summary['label'] == 'EM']
+
+    fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+    sns.barplot(x="day", y="mean", hue="label", data=event_summary, ax=ax)
+
+    for day in days:
+        group1_count = group1_data[group1_data['day'] == day]['sum'].iloc[0]
+        group2_count = group2_data[group2_data['day'] == day]['sum'].iloc[0]
+        group1_total = group1_data[group1_data['day'] == day]['count'].iloc[0]
+        group2_total = group2_data[group2_data['day'] == day]['count'].iloc[0]
+
+        contingency_table = [[group1_count, group1_total - group1_count],
+                             [group2_count, group2_total - group2_count]]
+
+        info, _, p_value = auto_select_test(contingency_table)
+        print(f'{selected_column} day {day}: {info} p-value = {p_value:.3f}')
+
+        if p_value < 0.05:
+            star_marker = significance_markers(p_value)
+            max_ratio = max(group1_count / group1_total, group2_count / group2_total)
+            ax.text(day - 5, max_ratio + 0.02, star_marker, fontsize=20, horizontalalignment='center')
+
+    plt.title(selected_column.replace('_', ' ').title() + ' Comparison')
+    plt.legend(loc='upper left')
+    plt.ylabel('Event Percentage')
+    plt.ylim(0, 1)
+    plt.tight_layout()
+    plt.savefig(f'Figures/Stats/Quantization/{fish_type}/behaviour_pattern/'
+                f'{power}W_{selected_column}_comparison_event.png', dpi=300)
+
+
+def compare_value(df, selected_column):
+    days = df['day'].unique()
+    group1_data = df[df['label'] == 'Control'].copy()
+    group2_data = df[df['label'] == 'EM'].copy()
+
+    fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+    sns.boxplot(x="day", y=selected_column, hue="label", data=df, ax=ax)
+
+    for day in days:
+        # compare values between labels
+        group1_value = group1_data[group1_data['day'] == day][selected_column].dropna()
+        group2_value = group2_data[group2_data['day'] == day][selected_column].dropna()
+
+        if len(group1_value) == 0 or len(group2_value) == 0:
+            continue
+        else:
+            _, p_value = mannwhitneyu(group1_value, group2_value)
+            print(f'{selected_column} day {day}: p-value = {p_value:.3f}')
+
+        if p_value < 0.05:
+            star_marker = significance_markers(p_value)
+            max_ratio = max(group1_value.mean(), group2_value.mean())
+            ax.text(day - 5, max_ratio + 0.02, star_marker, fontsize=20, horizontalalignment='center')
+
+    plt.title(selected_column.replace('_', ' ').title() + ' Comparison')
+    plt.legend(loc='upper left')
+    plt.ylabel('')
+    plt.tight_layout()
+    plt.savefig(f'Figures/Stats/Quantization/{fish_type}/behaviour_pattern/'
+                f'{power}W_{selected_column}_comparison_value.png', dpi=300)
 
 
 if __name__ == '__main__':
-    ACTIVITY_TYPE = 'burdur'
+    os.chdir('/Users/panpan/PycharmProjects/old_project/fish_llr')
     fish_type = 'Tg'
-    POWER = 1
-    batches = [1, 2]
-    days = [5, 6, 7, 8]
-    on_off_duration = 1800
-    pre_duration = 600
-    post_duration = 1770
-    colors = ['orange', 'blue']
-    compare_group = get_compare_group(POWER)
+    powers = [1, 1.2]
+    for power in powers:
+        features = pd.read_csv(f'Processed_data/behaviour_pattern/{fish_type}/{power}W_features.csv')
+        features['label'] = features['label'].apply(lambda x: 'Control' if x == 0 else 'EM')
 
-    for day in days:
-        df_batches = group_batch_data(fish_type, ACTIVITY_TYPE, POWER, batches, day)
+        # ====== comparing none vs not none between labels ==================================
+        compare_event(features, selected_column='startle_latency')
+        compare_event(features, selected_column='adjustment_interval')
+        compare_event(features, selected_column='active_bout_intensity')
+        compare_event(features, selected_column='rest_interval')
+        compare_event(features, selected_column='rest_bout_intensity')
 
-        # stimulus timestamp
-        on_off = [3601, 7201]
-        # metrics: startle response (latency and maximum); active bouts (latency, duration, and intensity)
-        # Transition from light OFF to ON: When the light is suddenly turned on after a period of darkness,
-        # zebrafish often display a startle response, characterized by a brief burst of intense locomotor activity.
-        # This response is thought to be a defensive reaction to sudden changes in the environment.
-        # After the initial startle response, the zebrafish gradually adjust to the light conditions and
-        # return to their typical active phase behavior.
-
-        off_on = [1801, 5401]
-        # metrics: Brief increase of activity after light off (latency and maximum);
-        # metrics (2): rest bouts (latency, duration, and intensity)
-        #  When the light is turned off after a period of illumination, zebrafish may initially show a brief increase in
-        #  activity, possibly as a response to the sudden change in their environment. However, they soon adjust to the
-        #  dark conditions and enter the rest phase, exhibiting reduced locomotor activity.
-
-        # ===== normalizing data, and remove baseline ===========================================
-        # step-one: remove baseline by pre-stimulus period
-        df_batches = rm_animal_baseline(df_batches, (off_on[0] - pre_duration, off_on[0]))
-
-        # ===== visualize data for individual fish =============================================
-        '''
-        import seaborn as sns
-        sns.set_style('whitegrid')
-        sns.set_context('paper')
-        sns.lineplot(data=df_batches, x='end', y='activity_sum_scaled', hue='label', palette='Set2')
-        '''
-
-        # step-two: remove baseline by batch mean
-        # df_batches = rm_batch_baseline(df_batches)
-
-        # step-three: remove baseline by light intensity
-
-        # ===== startle response after light ON ================================================
-        # metrics: startle response (latency and maximum), and adjust to light interval
-        startle_intensities, startle_latencies, adjustment_intervals, active_bout_intensities, active_bout_counts \
-            = measure_startle_response(df_batches, on_off[0], startle_threshold=3, startle_window=5,
-                                       stable_threshold=2, activity_threshold=0.1, on_window=on_off_duration,
-                                       min_stable_duration=2)
-
-        # ===== startle response after light OFF ===============================================
-        # metrics: brief increase of activity after light off (maximum), and adjust to dark interval
-        # metrics (2): rest bouts (latency, duration, and intensity)
-        increase_intensities, increase_latencies, rest_intervals, rest_bout_intensities, rest_bout_counts \
-            = measure_dark_adjustment_metrics(df_batches, off_on[0], activity_threshold=2, rest_threshold=2,
-                                              min_dark_stable_duration=2, off_window=on_off_duration)
-
-        # ===== Get features from feature dicts =============================================
-        features = pd.DataFrame([startle_intensities, startle_latencies, adjustment_intervals,
-                                 active_bout_intensities, active_bout_counts,
-                                 increase_intensities, increase_latencies, rest_intervals,
-                                 rest_bout_intensities, rest_bout_counts]).T
-
-        features.reset_index(inplace=True)
-        features.rename(columns={'index': 'animal_id'}, inplace=True)
-
-        features.columns = ['animal_id',
-                            'startle_intensity', 'startle_latency', 'adjustment_interval',
-                            'active_bout_intensity', 'active_bout_count',
-                            'increase_intensity', 'increase_latency', 'rest_interval',
-                            'rest_bout_intensity', 'rest_bout_count']
-
-        # sync label and batch from df_batches by animal_id
-        df_label_batch = df_batches[['animal_id', 'label', 'batch']].drop_duplicates()
-        features = pd.merge(features, df_label_batch, on='animal_id', how='left')
-
-
-        # ===== save features to csv file =============================================
-        features.to_csv(f'Processed_data/behaviour_pattern/{fish_type}/{POWER}W_day{day}_features.csv', index=False)
-
-        # ===== save features to csv file =============================================
-
-
+        # ====== comparing values between labels ==================================
+        compare_value(features, selected_column='adjustment_interval')
+        compare_value(features, selected_column='rest_bout_intensity')
+        compare_value(features, selected_column='rest_bout_count')
+        compare_value(features, selected_column='active_bout_intensity')
+        compare_value(features, selected_column='active_bout_count')
+        compare_value(features, selected_column='startle_latency')
